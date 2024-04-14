0db64f9bb79743fc7ab74624df78a092e9dd1b7b
Mateus Kasuya <mateusvbkasuya@gmail.com>
2024-04-14 13:16:31 -0300

    feat: implementar contrato

--- src/contrato.py
+++ src/contrato.py
@@ -1,10 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, PositiveFloat, PositiveInt, field_validator
 from datetime import datetime
+from enum import Enum
+
+class CategoriaEnum(str, Enum):
+    categoria1 = 'categoria1'
+    categoria2 = 'categoria2'
+    categoria3 = 'categoria3'
 
 class Vendas(BaseModel):
-    email : str
+    email : EmailStr
     data : datetime
-    valor : float
+    valor : PositiveFloat
     produto : str
-    quantidade : int
-    categoria : str
+    quantidade : PositiveInt
+    categoria : CategoriaEnum
+
+    @field_validator('categoria')
+    def categoria_deve_estar_no_enum(cls, error):
+        return error
